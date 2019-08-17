b73ae0eee85d4e42194525a4f220867abd706453
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-17 21:45:28 +0700

    Initial commit

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,17 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Muhammad Ashary on 17/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Muhammad Ashary on 17/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
